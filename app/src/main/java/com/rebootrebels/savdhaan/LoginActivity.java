88b127ef4ce07223f00429eb3ab4a040fa014f56
Keshav Jain <keshav.jain2354@gmail.com>
2020-11-20 11:21:53 +0530

    Initial commit

---
+++ app/src/main/java/com/rebootrebels/savdhaan/LoginActivity.java
@@ -0,0 +1,81 @@
+package com.rebootrebels.savdhaan;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText mEmail,mPassword;
+    Button mloginbtn;
+    TextView mcreatebtn;
+    private   FirebaseAuth fAuth;
+    ProgressBar pbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mEmail=(EditText)findViewById(R.id.editText);
+        mPassword=(EditText)findViewById(R.id.editText3);
+        mloginbtn=(Button)findViewById(R.id.button);
+        mcreatebtn=(TextView)findViewById(R.id.textView5);
+        pbar=(ProgressBar)findViewById(R.id.progressBar2);
+        fAuth=FirebaseAuth.getInstance();
+        mloginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                if (TextUtils.isEmpty(email)) {
+                    mEmail.setError("Email is Required");
+                    return;
+
+                }
+                if (TextUtils.isEmpty(password)){
+                    mEmail.setError("Password is Required");
+                    return;
+                }
+                if (password.length() < 6) {
+                    mPassword.setError("Password Must be >= 6 characters");
+                    return;
+                }
+                pbar.setVisibility(View.VISIBLE);
+                //Authenticate
+
+                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                        }
+                        else
+                        {
+                            Toast.makeText(LoginActivity.this,"Error" +task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+        mcreatebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Register.class));
+            }
+        });
+    }
+}
